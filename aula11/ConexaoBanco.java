b8d04046efe33927beecdc624f63553031c3876d
profralph <70354110+profralph@users.noreply.github.com>
2020-11-13 21:05:59 -0300

    Add files via upload

--- aula11/ConexaoBanco.java
+++ aula11/ConexaoBanco.java
@@ -9,69 +9,30 @@ public class ConexaoBanco {
 	public static void main(String[] args) {
 		
 		Connection con;
-		
-		List<Produto> produtos = new ArrayList<Produto>();
-		
+						
 		try {
 			
 			String url = "jdbc:sqlite:C:/TEMP/imed.db";
 			con = DriverManager.getConnection(url);
 			//System.out.println("Ok!");
 			
-			String sql = "SELECT id_produto, nome, preco FROM produto ";
+			ManterProduto mp = new ManterProduto(con);
+						
+			Produto p = new Produto();
+			p.setId(6);
+			p.setNome("Armário");
+			p.setPreco(560.2);
 			
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+			mp.inserirProduto(p);
+			
+			List<Produto> prods = mp.selecionarProdutos();
+			mp.mostrarProdutos(prods);
 									
-			while(rs.next()) {
-				
-				Produto p = new Produto();
-				
-				p.setId(rs.getInt("id_produto"));
-				p.setNome(rs.getString("nome"));
-				p.setPreco(rs.getDouble("preco"));
-				
-				produtos.add(p);
-			}
-			
-			/*String sqlIns = "INSERT INTO produto(id_produto, nome, preco) VALUES(5, 'Sofá', 750.34) ";
-			
-			stmt = con.createStatement();
-			stmt.executeQuery(sqlIns);*/
-			
-			String sqlIns = "INSERT INTO produto(id_produto, nome, preco) VALUES(?, ?, ?) ";
-			
-			PreparedStatement pstmt = con.prepareStatement(sqlIns);
-			pstmt.setInt(1, 5);
-			pstmt.setString(2, "Sofá");
-			pstmt.setDouble(3, 750.34);
-			
-			//pstmt.executeUpdate();
-			
-			String sqlUpd = "UPDATE produto SET preco = ? WHERE id_produto = ?";
-			
-			PreparedStatement pstmt2 = con.prepareStatement(sqlUpd);			
-			pstmt2.setDouble(1, 810.2);
-			pstmt2.setInt(2, 5);
-			
-			pstmt2.executeUpdate();
-			
-			String sqlDel = "DELETE FROM produto WHERE id_produto = ?";
-			
-			PreparedStatement pstmt3 = con.prepareStatement(sqlDel);			
-			pstmt3.setDouble(1, 5);
-			
-			//pstmt3.executeUpdate();
-			
 			con.close();
 			
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
-		}
-		
-		for (Produto p : produtos) {
-			System.out.println(p);
 		}
 	}
 }
---
+++ aula11/ManterProduto.java
@@ -0,0 +1,121 @@
+package imed;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManterProduto {
+
+	private Connection con;
+		
+	public ManterProduto(Connection con) {
+		
+		this.con = con;
+	} 
+	
+	public List<Produto> selecionarProdutos() {
+		
+		assert(con != null);
+		
+		List<Produto> produtos = new ArrayList<Produto>();
+		
+		String sql = "SELECT id_produto, nome, preco FROM produto ";
+		
+		try {
+		
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+		
+			while(rs.next()) {
+				
+				Produto p = new Produto();
+				
+				p.setId(rs.getInt("id_produto"));
+				p.setNome(rs.getString("nome"));
+				p.setPreco(rs.getDouble("preco"));
+				
+				produtos.add(p);
+			}
+			
+			stmt.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return produtos;
+	}
+	
+	public void mostrarProdutos(List<Produto> produtos) {
+		
+		for (Produto p : produtos) {
+			System.out.println(p);
+		}
+	}
+	
+	public void inserirProduto(Produto produto) {
+		
+		assert(con != null);
+		
+		try {
+			
+			String sqlIns = "INSERT INTO produto(id_produto, nome, preco) VALUES(?, ?, ?) ";
+			
+			PreparedStatement pstmt = con.prepareStatement(sqlIns);
+			pstmt.setInt(1, produto.getId());
+			pstmt.setString(2, produto.getNome());
+			pstmt.setDouble(3, produto.getPreco());
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void alterarProduto(Produto produto) {
+		
+		assert(con != null);
+		
+		String sqlUpd = "UPDATE produto SET preco = ?, nome = ? WHERE id_produto = ?";
+		
+		PreparedStatement pstmt;
+		
+		try {
+			
+			pstmt = con.prepareStatement(sqlUpd);
+			
+			pstmt.setDouble(1, produto.getPreco());
+			pstmt.setString(2, produto.getNome());
+			pstmt.setInt(3, produto.getId());
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}	
+	}
+	
+	public void excluirProduto(Produto produto) {
+		
+		assert(con != null);
+				
+		String sqlDel = "DELETE FROM produto WHERE nome = ?";
+		
+		try {
+			
+			PreparedStatement pstmt = con.prepareStatement(sqlDel);			
+			pstmt.setInt(1, produto.getId());		
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}	
+	}
+}
